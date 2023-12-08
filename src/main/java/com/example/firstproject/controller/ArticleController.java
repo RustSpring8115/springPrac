8e1d09796cd12badd3d424cb28992952995398d0
RustSpring8115 <rrws12@naver.com>
2023-12-08 23:06:07 +0900

    spring 복습

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -10,6 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -33,7 +36,7 @@ public class ArticleController {
         Article saved = articleRepository.save(article); // article 엔티티를 저장해 saved 객체에 반환
 //        System.out.println(saved.toString());
         log.info(saved.toString());
-        return "";
+        return "redirect:/articles/" + saved.getId();
     }
     @GetMapping("/articles/{id}")
     public String show(@PathVariable long id, Model model) {
@@ -44,5 +47,51 @@ public class ArticleController {
         model.addAttribute("article",articleEntitiy);
         //3. 뷰 페이지 반환하기
         return "articles/show";
+    }
+    @GetMapping("/articles")
+    public String index(Model model) {
+        //1. 모든 데이터 가져오기
+        List<Article> articleList = articleRepository.findAll();
+        log.info(articleList.toString());
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("articleList",articleList);
+        //3. 뷰 페이지 설정하기
+        return "articles/index";
+    }
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        //수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //모델에 데이터 등록하기
+        model.addAttribute("article",articleEntity);
+        //뷰 페이지 설정하기
+        return "articles/edit";
+    }
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        //1. DTO를 엔티티로 변환하기
+        Article articleEntity = form.toEntitiy();
+        log.info(articleEntity.toString());
+        //2. 엔티티를 DB에 저장하기
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        //2-2. 기존 데이터 값을 갱신하기
+        if (target != null) {
+            articleRepository.save(articleEntity); // 엔티티를 DB에 저장(갱신)
+        }
+        return "redirect:/articles/" + articleEntity.getId();
+    }
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info("삭제 요청이 들어왔습니다!!");
+        //1. 삭제할 대상 가져오기
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+        //2. 대상 엔티티 삭제하기
+        if (target != null) {
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg","삭제됐습니다.");
+        }
+        //3. 결과 페이지로 리다이렉트하기
+        return "redirect:/articles";
     }
 }
--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -3,5 +3,10 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entitiy.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
+
